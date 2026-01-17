ecfa462d2928aed859e62a7c2a9b8a9e28a4bce9
Thanh Vu <vu.nht00@gmail.com>
2026-01-18 03:23:53 +0700

    fix minor UI eror

--- src/features/word/components/RestartBtn.tsx
+++ src/features/word/components/RestartBtn.tsx
@@ -8,8 +8,11 @@ export default function RestartBtn() {
   }
 
   return (
-    <button className="button-restart w-fit" onClick={handleGetText}>
-      Restart Test <img src={iconRestart} alt="icon-restart" />
-    </button>
+    <section className="custom-container mb-300 flex flex-col flex-nowrap items-center gap-300 md:mb-500 lg:gap-400">
+      <hr className="w-full border-neutral-700" />
+      <button className="button-restart w-fit" onClick={handleGetText}>
+        Restart Test <img src={iconRestart} alt="icon-restart" />
+      </button>
+    </section>
   );
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,7 +6,7 @@ import WordContainer from '@/features/word/components/WordContainer';
 export default function Home() {
   return (
     <>
-      <section className="custom-container flex flex-col gap-400 py-400 md:py-500 lg:py-800">
+      <section className="custom-container flex flex-col gap-250 py-400 md:py-500 lg:py-800">
         <div
           id="info-config-bar"
           className="flex flex-col gap-y-200 xl:flex-row xl:justify-between"
@@ -16,10 +16,8 @@ export default function Home() {
         </div>
         <WordContainer />
       </section>
-      <section className="custom-container mb-300 flex flex-col flex-nowrap items-center gap-300 md:mb-500 lg:gap-400">
-        <hr className="w-full border-neutral-700" />
-        <RestartBtn />
-      </section>
+
+      <RestartBtn />
     </>
   );
 }
